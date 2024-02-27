ecce6e9e816d7889e245be863ac6d7d2d478b94b
Sara Isabel Ortiz <siortizh@eafit.edu.co>
2024-02-27 10:58:19 -0500

    Tercer tutorial

--- HELLOWORLD/pages/urls.py
+++ HELLOWORLD/pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import CartView, CartRemoveAllView, ImageViewFactory, ImageLocalStorage, ImageViewNoDI
 
 
 urlpatterns = [
@@ -9,4 +10,11 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('create/', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>/', ProductShowView.as_view(), name='show'),
-]  
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
+]
